d44848fa67ccf6f85c87b3fe8fab26fd47edf9e4
Manish Trivedi <manishtrivedi994@gmail.com>
2021-03-21 15:05:16 +0530

    Added inputs to Signup

--- view/src/components/Signup.js
+++ view/src/components/Signup.js
@@ -22,6 +22,14 @@ const  Signup = (props) => {
                 <input type="text" name="" required=""/>
                 <label>Phone</label>
               </div>
+              <div class="user-box">
+                <input type="password" name="" required=""/>
+                <label>Password</label>
+              </div>
+              <div class="user-box">
+                <input type="password" name="" required=""/>
+                <label>Confirm Password</label>
+              </div>
               <a href="/">
                 <span></span>
                 <span></span>
