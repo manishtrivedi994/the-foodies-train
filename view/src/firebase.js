f70bbab9bc2fce7d98f454a1749ce0ee9bfcb924
Amit Shekhar <shekharjvm@gmail.com>
2021-04-05 12:38:55 +0530

    bug fix

--- view/src/firebase.js
+++ view/src/firebase.js
@@ -1,20 +1,17 @@
-// import firebase from 'firebase/app';
-// import firebase from 'firebase'
-const firebase = require('firebase/app')
-// import 'firebase/auth';
-// import 'firebase/firestore';
-const dotenv = require('dotenv')
+import firebase from 'firebase/app'
+import 'firebase/auth';
+import 'firebase/firestore';
+// const dotenv = require('dotenv')
 
 dotenv.config({ path: './config_views.env' })
 console.log(process.env.APPID)
-
 const app = firebase.initializeApp({
-    apiKey: process.env.APIKEY,//"AIzaSyCdMCzZCguS6mYFkFPwrz4PvG9Qj2EM7d0",
-    authDomain: process.env.AUTHDOMAIN,//"the-foodies-train-8189c.firebaseapp.com",
-    projectId: process.env.PROJECTID,//"the-foodies-train-8189c",
-    storageBucket: process.env.STORAGEBUCKET,//"the-foodies-train-8189c.appspot.com",
-    messagingSenderId: process.env.MESSAGINGSENDERID,//"887212849566",
-    appId:process.env.APPID// "1:887212849566:web:729671066ce612bbaffe8f"
+    apiKey: "AIzaSyCdMCzZCguS6mYFkFPwrz4PvG9Qj2EM7d0",
+    authDomain:"the-foodies-train-8189c.firebaseapp.com",
+    projectId: "the-foodies-train-8189c",
+    storageBucket:"the-foodies-train-8189c.appspot.com",
+    messagingSenderId: "887212849566",
+    appId: "1:887212849566:web:729671066ce612bbaffe8f"
 })
 
 //not able to read from .env.local file
